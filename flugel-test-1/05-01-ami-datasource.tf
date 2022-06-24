1b0d84d062fd1caacfe1281151bc719bd9f249fe
Damilola Abioye <35523688+damyl4sure@users.noreply.github.com>
2022-06-24 09:30:00 +0100

    Update 05-01-ami-datasource.tf

--- flugel-test-1/05-01-ami-datasource.tf
+++ flugel-test-1/05-01-ami-datasource.tf
@@ -25,3 +25,26 @@ data "aws_ami" "amazon-linux2" {
 
 
 }
+
+# Get Ubuntu 22-04 AMI details 
+data "aws_ami" "ubuntu" {
+
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners = ["099720109477"]
+}
